134647b0eb1bdc3a80dc184a6a794226ad7d36e3
Matthew Leon <ml@matthewleon.com>
2017-12-15 17:50:10 -0500

    safety comments

--- src/Data/String/Format/Quick.purs
+++ src/Data/String/Format/Quick.purs
@@ -1,10 +1,9 @@
 module Data.String.Format.Quick (format) where
 
 import Data.Record.Unsafe (unsafeGet, unsafeHas)
-import Data.String.Regex (regex, replace')
+import Data.String.Regex (replace')
 import Data.String.Regex.Flags (global)
 import Data.String.Regex.Unsafe (unsafeRegex)
-import Partial.Unsafe (unsafePartial)
 import Type.Row.Homogeneous (class Homogeneous)
 
 -- | A quick and dirty (yet safe) format string.
@@ -14,11 +13,14 @@ import Type.Row.Homogeneous (class Homogeneous)
 -- | format "I like ${adj} ${noun}." {adj: "stinky", noun: "durians"}
 -- |   == "I like stinky durians."
 -- | ```
+-- |
+-- | Though I have yet to prove it to the type system, the `Homogeneous`
+-- | constraint below should make these operations safe.
 
 format :: forall r. Homogeneous r String => String -> Record r -> String
-format s r = replace' repRE (unsafePartial repFn) s
+format s r = replace' repRE repFn s
   where
   repRE = unsafeRegex "\\$\\{([a-z_].+?)\\}" global
 
-  repFn :: Partial => String -> Array String -> String
   repFn _ [match] | unsafeHas match r = unsafeGet match r
+  repFn _ _ = ""
