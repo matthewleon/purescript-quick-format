e091c1595cf8dac31b7ed3e1bf0f00206319d550
Matthew Leon <ml@matthewleon.com>
2017-12-15 17:43:47 -0500

    use unsafeRegex

--- src/Data/String/Format/Quick.purs
+++ src/Data/String/Format/Quick.purs
@@ -1,9 +1,9 @@
 module Data.String.Format.Quick (format) where
 
-import Data.Either (fromRight)
 import Data.Record.Unsafe (unsafeGet, unsafeHas)
 import Data.String.Regex (regex, replace')
 import Data.String.Regex.Flags (global)
+import Data.String.Regex.Unsafe (unsafeRegex)
 import Partial.Unsafe (unsafePartial)
 import Type.Row.Homogeneous (class Homogeneous)
 
@@ -18,7 +18,7 @@ import Type.Row.Homogeneous (class Homogeneous)
 format :: forall r. Homogeneous r String => String -> Record r -> String
 format s r = replace' repRE (unsafePartial repFn) s
   where
-  repRE = unsafePartial (fromRight (regex "\\$\\{([a-z_].+?)\\}" global))
+  repRE = unsafeRegex "\\$\\{([a-z_].+?)\\}" global
 
   repFn :: Partial => String -> Array String -> String
   repFn _ [match] | unsafeHas match r = unsafeGet match r
